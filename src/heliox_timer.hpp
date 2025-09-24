afd46a924466dbf6c285fb9085511b07b6336388
klaava <atterasanen5@gmail.com>
2025-09-24 12:38:50 +0300

    erased data

---
+++ src/heliox_timer.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <chrono>
+#include <functional>
+#include <initializer_list>
+
+
+template <typename ret, typename... param>
+using fn = std::function<ret(param...)>;
+
+template <typename ret, typename... param>
+double hx_timeit(fn<ret, param...> func, param&... elements)
+{
+	auto start = std::chrono::high_resolution_clock::now();
+	func(elements...);
+	auto elapsed = std::chrono::high_resolution_clock::now() - start;
+	long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+	return static_cast<double>(microseconds) * 0.000001;
+
+}
+
+
+
+
